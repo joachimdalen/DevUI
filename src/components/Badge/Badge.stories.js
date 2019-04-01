93ded01d1c552924fc7cd55591864b772bf913cd
Joachim Dalen <me@d3v.no>
2019-04-01 18:57:34 +0200

    Update badge styling

--- src/components/Badge/Badge.stories.js
+++ src/components/Badge/Badge.stories.js
@@ -3,11 +3,36 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Badge } from "./Badge";
 import { text, boolean } from "@storybook/addon-knobs/react";
-
-storiesOf("Components/Badge", module).add("Default Badge", () => (
-  <Badge
-    label={text("label", "Active")}
-    disabled={boolean("disabled", false)}
-    onClick={() => alert("hello there")}
-  />
-));
+const variants = [
+  "primary",
+  "secondary",
+  "dark",
+  "light",
+  "danger",
+  "warning",
+  "blue",
+  "magenta"
+];
+storiesOf("Components/Badge", module)
+  .add("Default Badge", () => <Badge label={text("label", "Active")} />)
+  .add("Neon Badge", () => (
+    <Badge
+      label={text("label", "Active")}
+      neonMode={boolean("Neon Mode", true)}
+      variant={text("Variant", "blue")}
+    />
+  ))
+  .add("Variants", () => (
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div>
+        {variants.map(variant => {
+          return <Badge label={variant} variant={variant} />;
+        })}
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        {variants.map(variant => {
+          return <Badge label={variant} variant={variant} neonMode={true} />;
+        })}
+      </div>
+    </div>
+  ));
--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,13 +1,28 @@
 import * as React from "react";
 import cx from "classnames";
 export interface Props {
-  /** this dictates what the button will say  */
   label: string;
+  variant: string;
+  neonMode: boolean;
 }
 
 export class Badge extends React.Component<Props> {
   public render() {
-    const { label } = this.props;
-    return <span className={cx("cb-badge", "cb-badge-primary")}>{label}</span>;
+    const { label, variant, neonMode = false } = this.props;
+    return (
+      <span
+        className={cx(
+          "cb-badge",
+          {
+            [`cb-badge-${variant}`]: variant !== "default" && !neonMode
+          },
+          {
+            [`cb-badge-neon-${variant}`]: variant !== "default" && neonMode
+          }
+        )}
+      >
+        {label}
+      </span>
+    );
   }
 }
