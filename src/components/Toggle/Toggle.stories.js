dd379affeae54cf0013a6768fb1371a8b74a3aeb
Joachim Dalen <me@d3v.no>
2019-04-08 17:34:57 +0200

    Minor toggle changes

--- src/components/Toggle/Toggle.stories.js
+++ src/components/Toggle/Toggle.stories.js
@@ -3,6 +3,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Toggle } from "./Toggle";
 import { text, boolean } from "@storybook/addon-knobs/react";
+import { List } from "../List/List";
+import { ListItem } from "../List/ListItem";
 const variants = [
   "primary",
   "secondary",
@@ -35,6 +37,29 @@ storiesOf("Components/Toggle", module)
       size={text("Size", "large")}
       showLabels={boolean("Show labels", true)}
     />
+  ))
+  .add("With context", () => (
+    <List
+      title={text("Title", "List title", "List")}
+      bordered={boolean("Bordered", true, "Styling")}
+    >
+      <ListItem
+        title={text("Item title", "This is a long item title", "List Items")}
+        subtitle={text(
+          "Item subtitle",
+          "While this is a long item subtitle",
+          "List Items"
+        )}
+        icon={
+          <Toggle
+            toggled={boolean("Toggled", true)}
+            rounded={boolean("Rounded", false)}
+            showLabels={boolean("Show labels", true)}
+          />
+        }
+        iconPlacement="right"
+      />
+    </List>
   ))
   .add("Variants", () => (
     <div style={{ display: "flex", flexDirection: "column" }}>
