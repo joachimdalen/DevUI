5b5d201c11f78c31a0eac5e116686bed9d695bbd
Joachim Dalen <me@d3v.no>
2019-05-30 15:47:08 +0200

    Fix toggle props not needing to be required

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -2,15 +2,13 @@ import * as React from "react";
 import cx from "classnames";
 
 export interface Props {
-  className: string;
-  toggled: boolean;
-  showLabels: boolean;
-  rounded: boolean;
-  onText: string;
-  offText: string;
+  className?: string;
+  toggled?: boolean;
+  showLabels?: boolean;
+  rounded?: boolean;
   onToggle: () => any;
-  size: "normal" | "large";
-  variant: string;
+  size?: "normal" | "large";
+  variant?: string;
 }
 
 export class Toggle extends React.Component<Props> {
