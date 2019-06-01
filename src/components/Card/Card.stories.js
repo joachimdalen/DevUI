74b75ed1a3dfcc7b3ad045a8b2f6eb31c9c2c27b
Joachim Dalen <me@d3v.no>
2019-06-01 20:22:46 +0200

    Minor updates to card

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -51,16 +51,25 @@ storiesOf("Components/Card", module)
       imagePlacement="top"
       width={number("card width", 200)}
     >
-      <CardHeader
-        title={text(
-          "title",
-          "Hamburger Hamlet Location"
-        )}
-      />
+      <CardHeader title={text("title", "Hamburger Hamlet Location")} />
       <CardBody>
         <label htmlFor="">{text("content", "Hello")}</label>
       </CardBody>
       <CardFooter>H</CardFooter>
+    </Card>
+  ))
+  .add("Top image 2", () => (
+    <Card
+      image={text(
+        "image",
+        "https://brdv.coffee/storage/uploads/3/sccc.s1nTQQq8KMWakWcgAwH.png"
+      )}
+      imagePlacement="top"
+      width={number("card width", 200)}
+    >
+      <CardBody>
+        <label htmlFor="">{text("content", "Hello")}</label>
+      </CardBody>
     </Card>
   ))
   .add("Left image", () => (
@@ -83,7 +92,8 @@ storiesOf("Components/Card", module)
       </CardBody>
       <CardFooter>H</CardFooter>
     </Card>
-  )).add("Right image", () => (
+  ))
+  .add("Right image", () => (
     <Card
       image={text(
         "image",
