90248595e9827804b62d0ca8e638421a2a4920cb
Joachim Dalen <me@d3v.no>
2019-06-03 07:18:54 +0200

    Update badge and global styles

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -4,17 +4,16 @@ import { isUndefined } from "util";
 import FontAwesomeIcon from "../FontAwesomeIcon/FontAwesomeIcon";
 export interface Props {
   label: string;
-  variant: string;
-  neonMode: boolean;
-  dismissible: boolean;
-  dismissText: string | React.ReactNode;
-  onDismiss: () => any;
+  variant?: string;
+  neonMode?: boolean;
+  dismissible?: boolean;
+  dismissText?: string | React.ReactNode;
+  onDismiss?: () => any;
 }
 
 export class Badge extends React.Component<Props> {
   public render() {
     const { label, variant, dismissible, onDismiss, dismissText } = this.props;
-    console.log(this.props);
     const badgeClass = cx(
       "dui-badge",
       { [`dui-badge-${variant}`]: variant !== "default" },
@@ -33,7 +32,10 @@ export class Badge extends React.Component<Props> {
       return (
         <div className={wrapperClass}>
           {badgeComponent}
-          <span className={dismissibleClass} onClick={() => onDismiss()}>
+          <span
+            className={dismissibleClass}
+            onClick={() => onDismiss && onDismiss()}
+          >
             {dismissTextComponent}
           </span>
         </div>
