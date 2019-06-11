775be3a103523e3a4ffbbd2ca58cf2a919a9c5a8
Joachim Dalen <me@d3v.no>
2019-06-11 07:46:53 +0200

    Add option to remove padding from Card

--- src/components/Card/CardBody.tsx
+++ src/components/Card/CardBody.tsx
@@ -2,11 +2,22 @@ import * as React from "react";
 import cx from "classnames";
 export interface Props {
   className?: string;
+  noPadding?: boolean;
 }
 
 export class CardBody extends React.Component<Props> {
   public render() {
-    const { children, className } = this.props;
-    return <div className={cx("dui-card-body", className)}>{children}</div>;
+    const { children, className, noPadding = false } = this.props;
+    return (
+      <div
+        className={cx(
+          "dui-card-body",
+          { "dui-card-body-np": noPadding },
+          className
+        )}
+      >
+        {children}
+      </div>
+    );
   }
 }
