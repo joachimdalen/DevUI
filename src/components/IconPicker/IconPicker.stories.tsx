a61510a1b3ad7bb7bd23c9dead1586fbc80f1a85
Joachim Dalen <me@d3v.no>
2019-06-15 07:09:08 +0200

    Add start of IconPicker

---
+++ src/components/IconPicker/IconPicker.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { IconPicker } from './IconPicker';
+import { action } from '@storybook/addon-actions';
+storiesOf("Components/IconPicker", module)
+    .add("Default", () => <IconPicker onIconSelect={action("onIconSelect")} />);
---
+++ src/components/IconPicker/IconPicker.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import cx from 'classnames';
+import { TextInput } from 'components/TextInput/TextInput';
+
+export interface IconPickerState {
+    query: string;
+    picked: Icon;
+}
+export interface Props {
+    onIconSelect: (icon: Icon) => void;
+    hiddenIcons?: string[];
+    paginate?: boolean;
+}
+export interface Icon {
+    prefix: "fas" | "far" | "fab";
+    icon: string;
+}
+
+const icons: Icon[] =
+    [{
+        prefix: "fas",
+        icon: "fa-check"
+    }];
+
+export class IconPicker extends React.Component<Props>{
+    render() {
+        return <div className={cx('dui-icon-picker')}>
+            <div className="dui-icon-picker-selector">d</div>
+            <div className="dui-icon-picker-container">
+                <div className="dui-icon-picker-search">
+                    <TextInput name="search" value="" placeholder="Search" onChange={() => console.log(1)}></TextInput>
+                </div>
+                <div className="dui-icon-picker-icons">
+                    {icons.filter(this._isHidden).map((i: Icon) => {
+                        return <span className="dui-icon-picker-icon" onClick={() => this._selectIcon(i)}>
+                            <i className={`${i.prefix} ${i.icon}`}></i>
+                        </span>
+                    })}
+                </div>
+
+            </div>
+        </div>
+    }
+    _isHidden(icon: Icon) {
+        const { hiddenIcons } = this.props;
+        if (!hiddenIcons) return false;
+        return hiddenIcons.findIndex(i => i == icon.icon) !== -1;
+    }
+    _selectIcon(icon: Icon) {
+        console.log("Selected icon" + icon.icon);
+    }
+}
