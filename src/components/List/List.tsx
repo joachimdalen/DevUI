1de4c702400275d4d56d52d213e134f374af57a4
Joachim Dalen <me@d3v.no>
2019-06-20 21:43:48 +0200

    Make list border optional

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,12 +3,15 @@ import cx from "classnames";
 
 export interface Props {
   title: string;
-  bordered: boolean;
+  bordered?: boolean;
 }
 
 export class List extends React.Component<Props> {
+  static defaultProps: Partial<Props> = {
+    bordered: false
+  };
   public render() {
-    const { children, title, bordered = false } = this.props;
+    const { children, title, bordered } = this.props;
     return (
       <div className={cx("dui-list-container")}>
         <p className={cx("dui-list-title")}>{title}</p>
