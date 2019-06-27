4f174608f90df035f2816e8533a96779e7e7910f
Joachim Dalen <me@d3v.no>
2019-06-27 09:50:30 +0200

    Remove IconPicker

--- src/components/IconPicker/IconPicker.stories.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { IconPicker } from './IconPicker';
-import { action } from '@storybook/addon-actions';
-storiesOf("Components/IconPicker", module)
-    .add("Default", () => <IconPicker onIconSelect={action("onIconSelect")} />);
