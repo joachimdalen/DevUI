a32737638525e27ff51513e7ddf3121cccb3bac8
Joachim Dalen <me@d3v.no>
2019-06-29 08:03:50 +0200

    Update Accordion

--- src/components/Accordion/Accordion.stories.js
+++ src/components/Accordion/Accordion.stories.js
@@ -1,11 +1,28 @@
 import React from "react";
-
 import { storiesOf } from "@storybook/react";
 import { Accordion } from "./Accordion";
-
 import { text } from "@storybook/addon-knobs/dist/deprecated";
-import { number } from "@storybook/addon-knobs";
+import { boolean } from "@storybook/addon-knobs";
+import FontAwesomeIcon from "../FontAwesomeIcon/FontAwesomeIcon";
 
-storiesOf("Components/Accordion", module).add("Basic Accordion", () => (
-  <Accordion>Hello</Accordion>
-));
+storiesOf("Components/Accordion", module)
+  .add("Basic Accordion", () => (
+    <Accordion
+      title={text("Account", "Account")}
+      initiallyExpanded={boolean("Initially Expanded", true)}
+    >
+      Hello
+    </Accordion>
+  ))
+  .add("Custom Title", () => (
+    <Accordion
+      title={
+        <span className="dui-color-danger">
+          <FontAwesomeIcon icon="fas fa-trash" margin marginDirection="right" />
+          Delete
+        </span>
+      }
+    >
+      Hello
+    </Accordion>
+  ));
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,21 +1,56 @@
 import * as React from "react";
 import cx from "classnames";
+import FontAwesomeIcon from "../FontAwesomeIcon/FontAwesomeIcon";
 
-export interface Props {
-  title: string;
+export interface AccordionProps {
+  title: string | React.ReactElement;
+  expandIcon?: string | React.ReactElement;
+  collapseIcon?: string | React.ReactElement;
+  initiallyExpanded?: boolean;
+}
+interface AccordionState {
   expanded: boolean;
 }
 
-export class Accordion extends React.Component<Props> {
+export class Accordion extends React.Component<AccordionProps, AccordionState> {
+  state = {
+    expanded: this.props.initiallyExpanded || false
+  };
   public render() {
-    const { children } = this.props;
+    const { children, title, expandIcon, collapseIcon } = this.props;
+    const { expanded } = this.state;
+    const caretIcon = (
+      <FontAwesomeIcon
+        icon={expanded ? "fas fa-caret-up" : "fas fa-caret-down"}
+      />
+    );
+    const expandComp = React.isValidElement(expandIcon)
+      ? expandIcon
+      : caretIcon;
+    const collapseComp = React.isValidElement(collapseIcon)
+      ? collapseIcon
+      : caretIcon;
     return (
       <div className={cx("dui-accordion")}>
-        <div className="dui-accordion-header">
-          <p className="dui-accordion-header-title">Hello</p>
-          <span className="dui-accordion-header-toggle">_</span>
+        <div
+          className="dui-accordion-header"
+          onClick={() => this.setState({ expanded: !expanded })}
+        >
+          <p className="dui-accordion-header-content">{title}</p>
+          <span className="dui-accordion-header-toggle">
+            {expanded ? collapseComp : expandComp}
+          </span>
         </div>
-        <div className="dui-accordion-content">{children}</div>
+        <div
+          className={cx(
+            { "dui-show": expanded },
+            { "dui-hide": !expanded },
+            "dui-accordion-content",
+            { "dui-accordion-content-expanded": expanded }
+          )}
+        >
+          {children}
+        </div>
       </div>
     );
   }
