e875193cae2381e444b4f019098b7cda9abd46d7
Joachim Dalen <me@d3v.no>
2019-07-03 07:29:43 +0200

    Update FormGroup with error and extra type

--- src/components/FormGroup/FormGroup.stories.tsx
+++ src/components/FormGroup/FormGroup.stories.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import { FormGroup } from "./FormGroup";
+import {
+  FormGroup,
+  FormGroupRequiredType,
+  FormGroupExtraType
+} from "./FormGroup";
 import { TextInput } from "../TextInput/TextInput";
 import { action } from "@storybook/addon-actions";
 import { Button } from "../Button/Button";
+import { boolean, select, text } from "@storybook/addon-knobs";
 
 storiesOf("Components|FormGroup", module)
   .add("Default", () => (
@@ -11,11 +16,65 @@ storiesOf("Components|FormGroup", module)
       <TextInput value="devexer" onChange={action("onChange")} />
     </FormGroup>
   ))
+  .add("Required", () => (
+    <FormGroup
+      label="Username"
+      required={boolean("required", true)}
+      requiredType={
+        select(
+          "Required Type",
+          ["icon", "text"],
+          "icon"
+        ) as FormGroupRequiredType
+      }
+    >
+      <TextInput value="devexer" onChange={action("onChange")} />
+    </FormGroup>
+  ))
   .add("Inline", () => (
     <FormGroup
       label="Username"
       inline
-      extra="This is the username you used when signing up"
+      extra={text("Extra", "This is the username you used when signing up")}
+    >
+      <TextInput value="devexer" onChange={action("onChange")} />
+      <Button
+        size="small"
+        icon="fas fa-save"
+        variant="success"
+        onClick={action("onClick")}
+      />
+    </FormGroup>
+  ))
+  .add("Extra", () => (
+    <FormGroup
+      label="Username"
+      inline
+      extra={text("Extra", "This is the username you used when signing up")}
+      extraType={
+        select(
+          "ExtraType",
+          ["success", "normal", "danger", "warning"],
+          "normal"
+        ) as FormGroupExtraType
+      }
+    >
+      <TextInput value="devexer" onChange={action("onChange")} />
+      <Button
+        size="small"
+        icon="fas fa-save"
+        variant="success"
+        onClick={action("onClick")}
+      />
+    </FormGroup>
+  ))
+  .add("With Error", () => (
+    <FormGroup
+      label="Username"
+      inline
+      extra={text("Extra", "This is the username you used when signing up")}
+      error={{ code: 400, message: "Username is already taken" }}
+      errorAccessor={error => error.message}
     >
       <TextInput value="devexer" onChange={action("onChange")} />
       <Button
--- src/components/FormGroup/FormGroup.tsx
+++ src/components/FormGroup/FormGroup.tsx
@@ -1,35 +1,82 @@
 import * as React from "react";
 import cx from "classnames";
-export interface Props {
+export interface FormGroupProps {
   label: string | React.ReactElement;
   inline?: boolean;
+  required?: boolean;
+  requiredType?: FormGroupRequiredType;
   extra?: string;
-  extraType?: "success" | "danger" | "warning" | "normal";
+  extraType?: FormGroupExtraType;
   className?: string;
+  error?: any;
+  errorAccessor?: (error: any) => string;
 }
-export class FormGroup extends React.Component<Props> {
-  static defaultProps: Partial<Props> = {
+export type FormGroupRequiredType = "icon" | "text";
+export type FormGroupExtraType = "success" | "danger" | "warning" | "normal";
+export class FormGroup extends React.Component<FormGroupProps> {
+  static defaultProps: Partial<FormGroupProps> = {
     inline: false,
-    extraType: "normal"
+    extraType: "normal",
+    requiredType: "icon"
   };
   render() {
-    const { label, extra, className, inline, children } = this.props;
+    const {
+      label,
+      extra,
+      extraType,
+      className,
+      inline,
+      children,
+      required,
+      requiredType,
+      error,
+      errorAccessor
+    } = this.props;
     const labelComponent = React.isValidElement(label) ? (
       label
     ) : (
-      <label className="dui-form-group-label">{label}</label>
+      <label className="dui-form-group-label">
+        {label}
+        {required && (
+          <span
+            className={cx("dui-form-group-required", {
+              "dui-form-group-required-text": requiredType === "text"
+            })}
+          >
+            {requiredType === "icon" ? "*" : "Required"}
+          </span>
+        )}
+      </label>
     );
+    
     const bodyComp = inline ? (
       <div className="dui-form-group-inline">{children}</div>
     ) : (
       children
+    );
+    
+    const errorComp = error && errorAccessor && (
+      <small className="dui-form-group-error dui-form-group-extra">
+        {errorAccessor(error)}
+      </small>
+    );
+    
+    const extraComp = extra && !error && (
+      <small
+        className={cx("dui-form-group-extra", {
+          [`dui-form-group-extra-${extraType}`]: extraType !== "normal"
+        })}
+      >
+        {extra}
+      </small>
     );
 
     return (
       <div className={cx("dui-form-group", className)}>
         {labelComponent}
         {bodyComp}
-        {extra && <small className="dui-form-group-extra">{extra}</small>}
+        {extraComp}
+        {errorComp}
       </div>
     );
   }
