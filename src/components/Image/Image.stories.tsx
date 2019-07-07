2a8c5e11125780fe5d5993dc65ffd6ea9e73748f
Joachim Dalen <me@d3v.no>
2019-07-07 14:49:02 +0200

    Update formatting

--- src/components/Image/Image.stories.tsx
+++ src/components/Image/Image.stories.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 
-import { Image } from './Image';
+import { Image } from "./Image";
 import { text } from "@storybook/addon-knobs";
 storiesOf("Display Components|Image", module)
-    .add("Default", () => (
-        <Image src={text('src', 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png')} alt="image" />
-    )).add("Fallback", () => (
-        <Image
-            src={text('src', 'http://localhost/img')}
-            fallbackSrc={text('fallbackSrc', 'https://placehold.it/200x200')}
-            alt="image" />
-    ));
+  .add("Default", () => (
+    <Image
+      src={text(
+        "src",
+        "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+      )}
+      alt="image"
+    />
+  ))
+  .add("Fallback", () => (
+    <Image
+      src={text("src", "http://localhost/img")}
+      fallbackSrc={text("fallbackSrc", "https://placehold.it/200x200")}
+      alt="image"
+    />
+  ));
--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,26 +1,42 @@
-import * as React from 'react';
-import cx from 'classnames';
+import * as React from "react";
+import cx from "classnames";
 
 export interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
-    className?: string;
-    src: string;
-    alt: string;
-    fallbackSrc?: string;
+  className?: string;
+  src: string;
+  alt: string;
+  fallbackSrc?: string;
 }
 interface ImageState {
-    failed: boolean;
+  failed: boolean;
 }
-export class Image extends React.Component<ImageProps, ImageState>{
-    state = {
-        failed: false
-    };
+export class Image extends React.Component<ImageProps, ImageState> {
+  state = {
+    failed: false
+  };
 
-    _failbackCall = () => {
-        this.setState({ failed: true });
-    }
-    render() {
-        const { src, alt, fallbackSrc, className, ...rest } = this.props;
-        if (this.state.failed && fallbackSrc) return <img src={fallbackSrc} alt={alt} className={cx('dui-image', className)} {...rest} />
-        return <img src={src} alt={alt} onError={this._failbackCall} className={cx('dui-image', className)} {...rest} />
-    }
+  _failbackCall = () => {
+    this.setState({ failed: true });
+  };
+  render() {
+    const { src, alt, fallbackSrc, className, ...rest } = this.props;
+    if (this.state.failed && fallbackSrc)
+      return (
+        <img
+          src={fallbackSrc}
+          alt={alt}
+          className={cx("dui-image", className)}
+          {...rest}
+        />
+      );
+    return (
+      <img
+        src={src}
+        alt={alt}
+        onError={this._failbackCall}
+        className={cx("dui-image", className)}
+        {...rest}
+      />
+    );
+  }
 }
