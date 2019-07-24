ffab358ef757664c8138496c767abdbc05d67839
Joachim Dalen <me@d3v.no>
2019-07-24 10:54:01 +0200

    Add className to SelectOption

--- src/components/Select/SelectOption.tsx
+++ src/components/Select/SelectOption.tsx
@@ -1,19 +1,21 @@
 import * as React from "react";
 import { SelectConsumer, SelectContextType } from "./SelectTypes";
+import cx from 'classnames';
 export interface Props {
   label: string;
   value: string;
   meta?: any;
+  className?: string;
   onClick?: (value: any) => any;
 }
 export class SelectOption extends React.PureComponent<Props> {
   render() {
-    const { children, label, value, meta } = this.props;
+    const { children, label, value, meta, className } = this.props;
     return (
       <SelectConsumer>
         {({ onSelect }: SelectContextType) => (
           <div
-            className="dui-select-option"
+            className={cx('dui-select-option', className)}
             role="option"
             onClick={() => {
               onSelect &&
