e5fc91c691c453d0c19ddc3365768826437203ac
Joachim Dalen <me@d3v.no>
2019-10-02 19:25:47 +0200

    Update Badge tests

--- src/components/Badge/Badge.test.tsx
+++ src/components/Badge/Badge.test.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import { Badge } from './Badge';
-import renderer from 'react-test-renderer';
+import React from "react";
+import { Badge } from "./Badge";
+import renderer from "react-test-renderer";
 
-describe('Badge', () => {
-    test('Badge renderes', () => {
-        const component = renderer.create(
-            <Badge label="Hello" />
-        );
-        let tree = component.toJSON();
-        expect(tree).toMatchSnapshot();
+describe("Badge", () => {
+  it("matches snapshot", () => {
+    const component = renderer.create(<Badge label="Hello" />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it("can be dismissed", () => {
+    const onDismiss = jest.fn();
+
+    const component = renderer.create(
+      <Badge label="Hello" variant="primary" onDismiss={onDismiss} />
+    );
+
+    let dismissButton = component.root.findByProps({
+      className: "dui-badge-dismissible"
     });
-    test('Badge can be dismissed', () => {
-        const onDismiss = jest.fn();
-
-        const component = renderer.create(<Badge label="Hello" dismissible onDismiss={onDismiss}></Badge>)
-
-
-        expect(onDismiss).toBeCalled();
-    })
-})
+    dismissButton.props.onClick();
+    expect(onDismiss).toBeCalled();
+  });
+  it("has the correct label", () => {
+    const label = "Badge";
+    const component = renderer.create(
+      <Badge label={label} variant="primary" />
+    );
+    expect(component.root.findByType(Badge).props.label).toBe(label);
+  });
+});
