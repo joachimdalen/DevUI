eb8978e8e45a38fa79268e4bc013428782a2843e
Joachim Dalen <me@d3v.no>
2019-10-10 20:08:39 +0200

    Add package build config

---
+++ index.tsx
@@ -0,0 +1,76 @@
+export {
+  default as FontAwesomeIcon
+} from "./src/components/FontAwesomeIcon/FontAwesomeIcon";
+export {
+  SocialButton,
+  SocialProviders
+} from "./src/components/SocialButton/SocialButton";
+export { Accordion } from "./src/components/Accordion/Accordion";
+export { Alert } from "./src/components/Alert/Alert";
+export { Avatar } from "./src/components/Avatar/Avatar";
+export { Badge } from "./src/components/Badge/Badge";
+export { Breadcrumb } from "./src/components/Breadcrumb/Breadcrumb";
+export { BreadcrumbItem } from "./src/components/Breadcrumb/BreadcrumbItem";
+export { Button } from "./src/components/Button/Button";
+export { ButtonGroup } from "./src/components/Button/ButtonGroup";
+export { Card } from "./src/components/Card/Card";
+export { CardBody } from "./src/components/Card/CardBody";
+export { CardFooter } from "./src/components/Card/CardFooter";
+export { CardHeader } from "./src/components/Card/CardHeader";
+export { CardImage } from "./src/components/Card/CardImage";
+export { CheckBox } from "./src/components/CheckBox/CheckBox";
+export { CheckBoxList } from "./src/components/CheckBoxList/CheckBoxList";
+export { DataTable } from "./src/components/Table/DataTable";
+export { Empty } from "./src/components/Empty/Empty";
+export { FormGroup } from "./src/components/FormGroup/FormGroup";
+export { Header } from "./src/components/Header/Header";
+export { HeaderBrand } from "./src/components/Header/HeaderBrand";
+export { HeaderItem } from "./src/components/Header/HeaderItem";
+export { HeaderUserMenu } from "./src/components/Header/HeaderUserMenu";
+export { HeaderUserMenuItem } from "./src/components/Header/HeaderUserMenuItem";
+export { Image } from "./src/components/Image/Image";
+export { List } from "./src/components/List/List";
+export { ListActionItem } from "./src/components/List/ListActionItem";
+export { ListItem } from "./src/components/List/ListItem";
+export { Modal, Props as ModalProps } from "./src/components/Modal/Modal";
+export { ModalBody } from "./src/components/Modal/ModalBody";
+export { ModalFooter } from "./src/components/Modal/ModalFooter";
+export { ModalHeader } from "./src/components/Modal/ModalHeader";
+export { NumberInput } from "./src/components/NumberInput/NumberInput";
+export { OverflowMenu } from "./src/components/OverflowMenu/OverflowMenu";
+export {
+  OverflowMenuItem
+} from "./src/components/OverflowMenu/OverflowMenuItem";
+export { Overlay } from "./src/components/Overlay/Overlay";
+export { Panel } from "./src/components/Panel/Panel";
+export { ProgressBar } from "./src/components/ProgressBar/ProgressBar";
+export { RadioButton } from "./src/components/RadioButton/RadioButton";
+export { Select } from "./src/components/Select/Select";
+export { SelectOption } from "./src/components/Select/SelectOption";
+export { SideBar } from "./src/components/SideBar/SideBar";
+export { SideBarAddon } from "./src/components/SideBar/SideBarAddon";
+export { SideBarMenu } from "./src/components/SideBar/SideBarMenu";
+export { SideBarMenuItem } from "./src/components/SideBar/SideBarMenuItem";
+export { TabControl } from "./src/components/TabControl/TabControl";
+export { Table } from "./src/components/Table/Table";
+export { TableCell } from "./src/components/Table/TableCell";
+export { TableColumnPicker } from "./src/components/Table/TableColumnPicker";
+export { TableHeader } from "./src/components/Table/TableHeader";
+export { TablePaginator } from "./src/components/Table/TablePaginator";
+export { TableRow } from "./src/components/Table/TableRow";
+export { TabNav } from "./src/components/TabControl/TabNav";
+export { TabNavItem } from "./src/components/TabControl/TabNavItem";
+export { TabPaneContainer } from "./src/components/TabControl/TabPaneContainer";
+export { TagInput } from "./src/components/TagInput/TagInput";
+export { TextArea } from "./src/components/TextArea/TextArea";
+export { TextInput } from "./src/components/TextInput/TextInput";
+export { Toast } from "./src/components/Toast/Toast";
+export { Toggle } from "./src/components/Toggle/Toggle";
+export { TreeView } from "./src/components/TreeView/TreeView";
+export { TreeViewItem } from "./src/components/TreeView/TreeViewItem";
+export * from "./src/components/common";
+export * from "./src/components/Header/HeaderTypes";
+export * from "./src/components/Select/SelectTypes";
+export * from "./src/components/TabControl/TabControlTypes";
+export * from "./src/components/Table/TableTypes";
+export * from "./src/components/TreeView/TreeViewTypes";
