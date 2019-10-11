5584e8eebf470df6f20fd7b58a49dbdc08a6c9fd
Joachim Dalen <me@d3v.no>
2019-10-11 19:53:04 +0200

    One more export try

--- index.tsx
+++ index.tsx
@@ -1,69 +1,348 @@
-export * from "./src/components/FontAwesomeIcon/FontAwesomeIcon";
-export * from "./src/components/SocialButton/SocialButton";
-export * from "./src/components/Accordion/Accordion";
-export * from "./src/components/Alert/Alert";
-export * from "./src/components/Avatar/Avatar";
-export * from "./src/components/Badge/Badge";
-export * from "./src/components/Breadcrumb/Breadcrumb";
-export * from "./src/components/Breadcrumb/BreadcrumbItem";
-export * from "./src/components/Button/Button";
-export * from "./src/components/Button/ButtonGroup";
-export * from "./src/components/Card/Card";
-export * from "./src/components/Card/CardBody";
-export * from "./src/components/Card/CardFooter";
-export * from "./src/components/Card/CardHeader";
-export * from "./src/components/Card/CardImage";
-export * from "./src/components/CheckBox/CheckBox";
-export * from "./src/components/CheckBoxList/CheckBoxList";
-export * from "./src/components/Table/DataTable";
-export * from "./src/components/Empty/Empty";
-export * from "./src/components/FormGroup/FormGroup";
-export * from "./src/components/Header/Header";
-export * from "./src/components/Header/HeaderBrand";
-export * from "./src/components/Header/HeaderItem";
-export * from "./src/components/Header/HeaderUserMenu";
-export * from "./src/components/Header/HeaderUserMenuItem";
-export * from "./src/components/Image/Image";
-export * from "./src/components/List/List";
-export * from "./src/components/List/ListActionItem";
-export * from "./src/components/List/ListItem";
-export * from "./src/components/Modal/Modal";
-export * from "./src/components/Modal/ModalBody";
-export * from "./src/components/Modal/ModalFooter";
-export * from "./src/components/Modal/ModalHeader";
-export * from "./src/components/NumberInput/NumberInput";
-export * from "./src/components/OverflowMenu/OverflowMenu";
-export * from "./src/components/OverflowMenu/OverflowMenuItem";
-export * from "./src/components/Overlay/Overlay";
-export * from "./src/components/Panel/Panel";
-export * from "./src/components/ProgressBar/ProgressBar";
-export * from "./src/components/RadioButton/RadioButton";
-export * from "./src/components/Select/Select";
-export * from "./src/components/Select/SelectOption";
-export * from "./src/components/SideBar/SideBar";
-export * from "./src/components/SideBar/SideBarAddon";
-export * from "./src/components/SideBar/SideBarMenu";
-export * from "./src/components/SideBar/SideBarMenuItem";
-export * from "./src/components/TabControl/TabControl";
-export * from "./src/components/Table/Table";
-export * from "./src/components/Table/TableCell";
-export * from "./src/components/Table/TableColumnPicker";
-export * from "./src/components/Table/TableHeader";
-export * from "./src/components/Table/TablePaginator";
-export * from "./src/components/Table/TableRow";
-export * from "./src/components/TabControl/TabNav";
-export * from "./src/components/TabControl/TabNavItem";
-export * from "./src/components/TabControl/TabPaneContainer";
-export * from "./src/components/TagInput/TagInput";
-export * from "./src/components/TextArea/TextArea";
-export * from "./src/components/TextInput/TextInput";
-export * from "./src/components/Toast/Toast";
-export * from "./src/components/Toggle/Toggle";
-export * from "./src/components/TreeView/TreeView";
-export * from "./src/components/TreeView/TreeViewItem";
-export * from "./src/components/common";
-export * from "./src/components/Header/HeaderTypes";
-export * from "./src/components/Select/SelectTypes";
-export * from "./src/components/TabControl/TabControlTypes";
-export * from "./src/components/Table/TableTypes";
-export * from "./src/components/TreeView/TreeViewTypes";
+import {
+  FontAwesomeIconProps,
+  FontAwesomeIcon
+} from "./src/components/FontAwesomeIcon/FontAwesomeIcon";
+import {
+  SocialProviders,
+  SocialButton,
+  SocialButtonProps
+} from "./src/components/SocialButton/SocialButton";
+import {
+  Accordion,
+  AccordionProps
+} from "./src/components/Accordion/Accordion";
+import { Alert, AlertProps, AlertVariant } from "./src/components/Alert/Alert";
+import {
+  Avatar,
+  AvatarFormat,
+  AvatarProps,
+  AvatarSize
+} from "./src/components/Avatar/Avatar";
+import { Badge, BadgeProps } from "./src/components/Badge/Badge";
+import {
+  Breadcrumb,
+  BreadcrumbProps
+} from "./src/components/Breadcrumb/Breadcrumb";
+import {
+  BreadcrumbItem,
+  BreadcrumbItemProps
+} from "./src/components/Breadcrumb/BreadcrumbItem";
+import {
+  Button,
+  ButtonFormat,
+  ButtonProps,
+  ButtonSize
+} from "./src/components/Button/Button";
+import {
+  ButtonGroup,
+  ButtonGroupProps
+} from "./src/components/Button/ButtonGroup";
+import {
+  Card,
+  CardImagePlacement,
+  CardProps
+} from "./src/components/Card/Card";
+import { CardBody, CardBodyProps } from "./src/components/Card/CardBody";
+import { CardFooter, CardFooterProps } from "./src/components/Card/CardFooter";
+import { CardHeader, CardHeaderProps } from "./src/components/Card/CardHeader";
+import { CardImage, CardImageProps } from "./src/components/Card/CardImage";
+import {
+  CheckBox,
+  CheckBoxIndicatorLocation,
+  CheckBoxProps
+} from "./src/components/CheckBox/CheckBox";
+import {
+  CheckBoxList,
+  CheckBoxListProps
+} from "./src/components/CheckBoxList/CheckBoxList";
+import { DataTable, DataTableProps } from "./src/components/Table/DataTable";
+import { Empty, EmptyProps } from "./src/components/Empty/Empty";
+import {
+  FormGroup,
+  FormGroupProps
+} from "./src/components/FormGroup/FormGroup";
+import { Header, HeaderProps } from "./src/components/Header/Header";
+import {
+  HeaderBrand,
+  HeaderBrandProps
+} from "./src/components/Header/HeaderBrand";
+import {
+  HeaderItem,
+  HeaderItemProps
+} from "./src/components/Header/HeaderItem";
+import {
+  HeaderUserMenu,
+  HeaderUserMenuProps
+} from "./src/components/Header/HeaderUserMenu";
+import {
+  HeaderUserMenuItem,
+  HeaderUserMenuItemProps
+} from "./src/components/Header/HeaderUserMenuItem";
+import { Image, ImageProps } from "./src/components/Image/Image";
+import { List, ListProps } from "./src/components/List/List";
+import {
+  ListActionItem,
+  ListActionItemProps
+} from "./src/components/List/ListActionItem";
+import { ListItem, ListItemProps } from "./src/components/List/ListItem";
+import {
+  Modal,
+  ModalLocation,
+  ModalProps,
+  ModalSize
+} from "./src/components/Modal/Modal";
+import { ModalBody, ModalBodyProps } from "./src/components/Modal/ModalBody";
+import {
+  ModalFooter,
+  ModalFooterProps
+} from "./src/components/Modal/ModalFooter";
+import {
+  ModalHeader,
+  ModalHeaderProps
+} from "./src/components/Modal/ModalHeader";
+import {
+  NumberInput,
+  NumberInputProps
+} from "./src/components/NumberInput/NumberInput";
+import {
+  OverflowMenu,
+  OverflowMenuLocation,
+  OverflowMenuProps
+} from "./src/components/OverflowMenu/OverflowMenu";
+import {
+  OverflowMenuItem,
+  OverflowMenuItemProps
+} from "./src/components/OverflowMenu/OverflowMenuItem";
+import { Overlay, OverlayProps } from "./src/components/Overlay/Overlay";
+import { Panel, PanelProps } from "./src/components/Panel/Panel";
+import {
+  ProgressBar,
+  ProgressBarProps
+} from "./src/components/ProgressBar/ProgressBar";
+import {
+  RadioButton,
+  RadioButtonProps
+} from "./src/components/RadioButton/RadioButton";
+import { Select, SelectProps } from "./src/components/Select/Select";
+import {
+  SelectOption,
+  SelectOptionProps
+} from "./src/components/Select/SelectOption";
+import {
+  SideBar,
+  SideBarAddonLocation,
+  SideBarProps
+} from "./src/components/SideBar/SideBar";
+import {
+  SideBarAddon,
+  SideBarAddonProps
+} from "./src/components/SideBar/SideBarAddon";
+import {
+  SideBarMenu,
+  SideBarMenuProps
+} from "./src/components/SideBar/SideBarMenu";
+import {
+  SideBarMenuItem,
+  SideBarMenuItemProps
+} from "./src/components/SideBar/SideBarMenuItem";
+import {
+  TabControl,
+  TabControlProps
+} from "./src/components/TabControl/TabControl";
+import { Table, TableProps } from "./src/components/Table/Table";
+import { TableCell, TableCellProps } from "./src/components/Table/TableCell";
+import {
+  TableColumnPicker,
+  TableColumnPickerProps
+} from "./src/components/Table/TableColumnPicker";
+import {
+  TableHeader,
+  TableHeaderProps
+} from "./src/components/Table/TableHeader";
+import {
+  TablePaginator,
+  TablePaginatorProps
+} from "./src/components/Table/TablePaginator";
+import { TableRow, TableRowProps } from "./src/components/Table/TableRow";
+import { TabNav, TabNavProps } from "./src/components/TabControl/TabNav";
+import { TabNavItem } from "./src/components/TabControl/TabNavItem";
+import { TabPaneContainer } from "./src/components/TabControl/TabPaneContainer";
+import {
+  Tag,
+  TagInput,
+  TagInputProps
+} from "./src/components/TagInput/TagInput";
+import {
+  TextArea,
+  TextAreaProps,
+  TextAreaSizeMode
+} from "./src/components/TextArea/TextArea";
+import {
+  TextInput,
+  TextInputProps
+} from "./src/components/TextInput/TextInput";
+import { Toast, ToastProps } from "./src/components/Toast/Toast";
+import {
+  Toggle,
+  ToggleProps,
+  ToggleSize
+} from "./src/components/Toggle/Toggle";
+import { TreeView, TreeViewProps } from "./src/components/TreeView/TreeView";
+import {
+  TreeViewItem,
+  TreeViewItemProps
+} from "./src/components/TreeView/TreeViewItem";
+import { CustomComponent, GenericSizes, Omit } from "./src/components/common";
+
+import { SelectOptionType } from "./src/components/Select/SelectTypes";
+import { TabType } from "./src/components/TabControl/TabControlTypes";
+import { Column } from "./src/components/Table/TableTypes";
+import { TreeViewNode } from "./src/components/TreeView/TreeViewTypes";
+
+export {
+  FontAwesomeIconProps,
+  FontAwesomeIcon,
+  SocialProviders,
+  SocialButton,
+  SocialButtonProps,
+  Accordion,
+  AccordionProps,
+  Alert,
+  AlertProps,
+  AlertVariant,
+  Avatar,
+  AvatarFormat,
+  AvatarProps,
+  AvatarSize,
+  Badge,
+  BadgeProps,
+  Breadcrumb,
+  BreadcrumbProps,
+  BreadcrumbItem,
+  BreadcrumbItemProps,
+  Button,
+  ButtonFormat,
+  ButtonProps,
+  ButtonSize,
+  ButtonGroup,
+  ButtonGroupProps,
+  Card,
+  CardImagePlacement,
+  CardProps,
+  CardBody,
+  CardBodyProps,
+  CardFooter,
+  CardFooterProps,
+  CardHeader,
+  CardHeaderProps,
+  CardImage,
+  CardImageProps,
+  CheckBox,
+  CheckBoxIndicatorLocation,
+  CheckBoxProps,
+  CheckBoxList,
+  CheckBoxListProps,
+  DataTable,
+  DataTableProps,
+  Empty,
+  EmptyProps,
+  FormGroup,
+  FormGroupProps,
+  Header,
+  HeaderProps,
+  HeaderBrand,
+  HeaderBrandProps,
+  HeaderItem,
+  HeaderItemProps,
+  HeaderUserMenu,
+  HeaderUserMenuProps,
+  HeaderUserMenuItem,
+  HeaderUserMenuItemProps,
+  Image,
+  ImageProps,
+  List,
+  ListProps,
+  ListActionItem,
+  ListActionItemProps,
+  ListItem,
+  ListItemProps,
+  Modal,
+  ModalLocation,
+  ModalProps,
+  ModalSize,
+  ModalBody,
+  ModalBodyProps,
+  ModalFooter,
+  ModalFooterProps,
+  ModalHeader,
+  ModalHeaderProps,
+  NumberInput,
+  NumberInputProps,
+  OverflowMenu,
+  OverflowMenuLocation,
+  OverflowMenuProps,
+  OverflowMenuItem,
+  OverflowMenuItemProps,
+  Overlay,
+  OverlayProps,
+  Panel,
+  PanelProps,
+  ProgressBar,
+  ProgressBarProps,
+  RadioButton,
+  RadioButtonProps,
+  Select,
+  SelectProps,
+  SelectOption,
+  SelectOptionProps,
+  SideBar,
+  SideBarAddonLocation,
+  SideBarProps,
+  SideBarAddon,
+  SideBarAddonProps,
+  SideBarMenu,
+  SideBarMenuProps,
+  SideBarMenuItem,
+  SideBarMenuItemProps,
+  TabControl,
+  TabControlProps,
+  Table,
+  TableProps,
+  TableCell,
+  TableCellProps,
+  TableColumnPicker,
+  TableColumnPickerProps,
+  TableHeader,
+  TableHeaderProps,
+  TablePaginator,
+  TablePaginatorProps,
+  TableRow,
+  TableRowProps,
+  TabNav,
+  TabNavProps,
+  TabNavItem,
+  TabPaneContainer,
+  Tag,
+  TagInput,
+  TagInputProps,
+  TextArea,
+  TextAreaProps,
+  TextAreaSizeMode,
+  TextInput,
+  TextInputProps,
+  Toast,
+  ToastProps,
+  Toggle,
+  ToggleProps,
+  ToggleSize,
+  TreeView,
+  TreeViewProps,
+  TreeViewItem,
+  TreeViewItemProps,
+  CustomComponent,
+  GenericSizes,
+  SelectOptionType,
+  TabType,
+  Column,
+  TreeViewNode
+};
