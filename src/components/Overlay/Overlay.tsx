70cf4d755d9e3f85b7a3cc7e05aee2d0cd4118da
Joachim Dalen <me@d3v.no>
2019-10-13 20:16:29 +0200

    Add configurable background color to Overlay

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import cx from "classnames";
+import { Color } from "csstype";
 export interface OverlayProps {
   className?: string;
   visible: boolean;
+  backgroundColor?: Color;
 }
 export const Overlay: React.FunctionComponent<OverlayProps> = ({
   children,
   visible,
-  className
+  className, backgroundColor
 }) => {
   const baseClass = cx("dui-overlay", className);
-  return visible ? <div className={baseClass}>{children}</div> : null;
+  return visible ? <div className={baseClass} style={{ backgroundColor: backgroundColor }}>{children}</div> : null;
 };
