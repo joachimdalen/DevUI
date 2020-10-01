aa1ab2fa196b292aa4a4478ad8af0113a9201456
Joachim Dalen <me@d3v.no>
2020-10-01 18:37:03 +0200

    Remove RibbonTab

--- src/components/Ribbon/RibbonTab.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export interface RibbonTabProps {
-  children: React.ReactElement | React.ReactElement[];
-}
-export const RibbonTab = ({ children }: RibbonTabProps) => {
-  return <div className="dui-ribbon-items"> {children}</div>;
-};
